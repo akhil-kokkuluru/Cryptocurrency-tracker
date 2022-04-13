9353d71d98117efcf0effb7a850da96cceb8b1ee
akhil <akhilr37@gmail.com>
2022-04-13 17:46:57 +0000

    first commit

---
+++ src/components/CryptocurrenciesList/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+const CryptocurrenciesList = () => (
+  <div className="ContentContainer">
+    <h1 className="heading">Cryptocurrency Tracker</h1>
+  </div>
+)
+
+export default CryptocurrenciesList
