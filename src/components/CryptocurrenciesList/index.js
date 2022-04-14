e772df4e0300d68fdf6280e4f04fdcfbb743060e
akhil <akhilr37@gmail.com>
2022-04-14 16:44:07 +0000

    second commit

--- src/components/CryptocurrenciesList/index.js
+++ src/components/CryptocurrenciesList/index.js
@@ -1,8 +1,28 @@
 import './index.css'
+import CryptocurrencyItem from '../CryptocurrencyItem'
 
 const CryptocurrenciesList = () => (
+  //   const currencyList = await fetch(
+  //     'https://apis.ccbp.in/crypto-currency-converter',
+  //   )
+  //   const listing = await currencyList.json()
   <div className="ContentContainer">
     <h1 className="heading">Cryptocurrency Tracker</h1>
+    <img
+      alt="cryptocurrency"
+      className="titleImg"
+      src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
+    />
+    <div className="currenciesContainer">
+      <div className="headingContainer">
+        <p className="HeadingCss">Coin Type</p>
+        <div className="currencyContainer">
+          <p className="HeadingCss">USD</p>
+          <p className="HeadingCss">EURO</p>
+        </div>
+      </div>
+      <CryptocurrencyItem />
+    </div>
   </div>
 )
 
--- src/components/CryptocurrencyItem/index.js
+++ src/components/CryptocurrencyItem/index.js
@@ -1,1 +1,13 @@
-// Write your JS code here
+import './index.css'
+
+const CryptocurrencyItem = () => (
+  <div className="headingContainer">
+    <p className="HeadingCss">Coin Type</p>
+    <div className="currencyContainer">
+      <p className="HeadingCss">USD</p>
+      <p className="HeadingCss">EURO</p>
+    </div>
+  </div>
+)
+
+export default CryptocurrencyItem
