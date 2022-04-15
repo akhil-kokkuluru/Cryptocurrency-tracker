07ce143e2ef8c02a93edfea85b857d42baa4cd9e
akhil <akhilr37@gmail.com>
2022-04-15 17:27:59 +0000

    all functionalities added

--- src/components/CryptocurrenciesList/index.js
+++ src/components/CryptocurrenciesList/index.js
@@ -1,29 +1,74 @@
 import './index.css'
+import Loader from 'react-loader-spinner'
+import {Component} from 'react'
 import CryptocurrencyItem from '../CryptocurrencyItem'
 
-const CryptocurrenciesList = () => (
-  //   const currencyList = await fetch(
-  //     'https://apis.ccbp.in/crypto-currency-converter',
-  //   )
-  //   const listing = await currencyList.json()
-  <div className="ContentContainer">
-    <h1 className="heading">Cryptocurrency Tracker</h1>
-    <img
-      alt="cryptocurrency"
-      className="titleImg"
-      src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
-    />
-    <div className="currenciesContainer">
-      <div className="headingContainer">
-        <p className="HeadingCss">Coin Type</p>
-        <div className="currencyContainer">
-          <p className="HeadingCss">USD</p>
-          <p className="HeadingCss">EURO</p>
-        </div>
-      </div>
-      <CryptocurrencyItem />
+class CryptocurrenciesList extends Component {
+  state = {isLoading: true, cryptoCureencyData: []}
+
+  componentDidMount() {
+    this.currenctListFetching()
+  }
+
+  currenctListFetching = async () => {
+    const currencyList = await fetch(
+      'https://apis.ccbp.in/crypto-currency-converter',
+    )
+    const listing = await currencyList.json()
+    this.setState({
+      cryptoCureencyData: listing.map(eachItem => ({
+        id: eachItem.id,
+        currencyLogo: eachItem.currency_logo,
+        currencyName: eachItem.currency_name,
+        euroValue: eachItem.euro_value,
+        usdValue: eachItem.usd_value,
+      })),
+      isLoading: false,
+    })
+  }
+
+  loadingRendering = () => (
+    <div testid="loader">
+      <Loader type="Rings" color="#ffffff" height={80} width={80} />
     </div>
-  </div>
-)
+  )
+
+  listRendering = () => {
+    const {cryptoCureencyData} = this.state
+
+    return cryptoCureencyData.map(item => (
+      <CryptocurrencyItem key={item.id} cryptoCureencyData={item} />
+    ))
+  }
+
+  render() {
+    const {isLoading} = this.state
+    const renderElements = () => (
+      <>
+        <h1 className="heading">Cryptocurrency Tracker</h1>
+        <img
+          alt="cryptocurrency"
+          className="titleImg"
+          src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
+        />
+        <ul className="currenciesContainer">
+          <div className="headingContainer">
+            <p className="HeadingCss">Coin Type</p>
+            <div className="currencyContainer">
+              <p className="HeadingCss">USD</p>
+              <p className="HeadingCss">EURO</p>
+            </div>
+          </div>
+          {this.listRendering()}
+        </ul>
+      </>
+    )
+    return (
+      <div className="ContentContainer">
+        {isLoading ? this.loadingRendering() : renderElements()}
+      </div>
+    )
+  }
+}
 
 export default CryptocurrenciesList
--- src/components/CryptocurrencyItem/index.js
+++ src/components/CryptocurrencyItem/index.js
@@ -1,13 +1,20 @@
 import './index.css'
 
-const CryptocurrencyItem = () => (
-  <div className="headingContainer">
-    <p className="HeadingCss">Coin Type</p>
-    <div className="currencyContainer">
-      <p className="HeadingCss">USD</p>
-      <p className="HeadingCss">EURO</p>
-    </div>
-  </div>
-)
+const CryptocurrencyItem = props => {
+  const {cryptoCureencyData} = props
+  const {currencyLogo, currencyName, euroValue, usdValue} = cryptoCureencyData
 
+  return (
+    <li className="CheadingContainer">
+      <div className="logoContainer">
+        <img className="logoCss" src={currencyLogo} alt={currencyName} />
+        <p className="CHeadingCss">{currencyName}</p>
+      </div>
+      <div className="CcurrencyContainer">
+        <p className="CHeadingCss">{usdValue}</p>
+        <p className="CHeadingCss">{euroValue}</p>
+      </div>
+    </li>
+  )
+}
 export default CryptocurrencyItem
